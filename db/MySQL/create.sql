56cf264af1e19c182fb48b4ecca1c08a56ee0ecd
Alvaro Magalhães <alvpmagalhaes@gmail.com>
2020-12-13 18:19:59 -0300

    update database

--- db/MySQL/create.sql
+++ db/MySQL/create.sql
@@ -1,32 +1,43 @@
-drop database Livraria;
+create database ProjetoWeb1;
+show databases;
+use ProjetoWeb1;
 
-create database Livraria;
+create table SiteVendaDeIngressos(
+    id bigint not null auto_increment,
+    nome varchar(256) not null,
+    email varchar(64) not null unique,
+    senha varchar(64) not null,
+    url varchar(64) not null,
+    telefone varchar(13),
+    primary key (id)
+);
 
-use Livraria;
+insert into SiteVendaDeIngressos(nome, email, senha, url, telefone) values ('Byma', 'byma@byma.com.br', 'byma', 'www.byma.com.br', '(16)4002-8922');
 
-create table Editora(id bigint not null auto_increment, cnpj varchar(18) not null, nome varchar(256) not null, primary key (id));
+create table SalasDeTeatro(
+    id bigint not null auto_increment,
+    cnpj varchar(18) not null,
+    nome varchar(256) not null,
+    email varchar(64) not null unique,
+    senha varchar(64) not null,
+    cidade varchar(64) not null,
+    primary key (id)
+);
 
-create table Livro(id bigint not null auto_increment, titulo varchar(256) not null, autor varchar(256) not null, ano integer not null, preco float not null, editora_id bigint not null, primary key (id), foreign key (editora_id) references Editora(id));
+insert into SalasDeTeatro(cnpj, nome, email, senha, cidade) values ('55.789.390/0008-99', 'Cine São Carlos', 'cinesaocarlos@saocarlos.com.br', 'cinesaocarlos', 'São Carlos');
 
-insert into Editora(cnpj, nome) values  ('55.789.390/0008-99', 'Companhia das Letras');
+ create table CadastroPromocoes(
+    id bigint not null auto_increment,
+    cnpj varchar(18) not null,
+    nome_peca varchar(256) not null,
+    url varchar(64) not null,
+    preco float not null,
+    dia varchar(10) not null,
+    horario varchar(5) not null,
+    primary key (id)
+);
 
-insert into Editora(cnpj, nome) values ('71.150.470/0001-40', 'Record');
+show databases;
 
-insert into Editora(cnpj, nome) values ('32.106.536/0001-82', 'Objetiva');
 
-insert into Livro(titulo, autor, ano, preco, editora_id) values ('Ensaio sobre a Cegueira', 'José Saramago', 1995, 54.9, 1);
-
-insert into Livro(titulo, autor, ano, preco, editora_id) values  ('Cem anos de Solidão', 'Gabriel Garcia Márquez', 1977, 59.9, 2);
-
-insert into Livro(titulo, autor, ano, preco, editora_id) values ('Diálogos Impossíveis', 'Luis Fernando Verissimo', 2012, 22.9, 3);
-
-create table Usuario(id bigint not null auto_increment, nome varchar(256) not null, login varchar(20) not null unique, senha varchar(64) not null, papel varchar(10), primary key (id));
-
-insert into Usuario(nome, login, senha, papel) values ('Administrador', 'admin', 'admin', 'ADMIN');
-
-insert into Usuario(nome, login, senha, papel) values ('Usuario', 'user', 'user', 'USER');
-
-create table Compra(id bigint not null auto_increment, data varchar(10) not null, valor float not null, livro_id bigint not null, usuario_id bigint not null, primary key (id), foreign key (livro_id) references Livro(id), foreign key (usuario_id) references Usuario(id));
-
-insert into Compra(data, valor, livro_id, usuario_id) values ('30/08/2020', 10.88, 1, 2);
-
+insert into CadastroPromocoes(cnpj, nome_peca, url, preco, dia, horario) values ('71.150.470/0001-40', 'Cisne Negro', 'www.cinesaocarlos.com.br', 5.00, '13/12/2020', '13:00');
