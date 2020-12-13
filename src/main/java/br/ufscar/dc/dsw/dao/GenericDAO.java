d3eb3bea53ae5e25c1ff5b5b46c3e9144b1b1eb1
Alvaro Magalhães <alvpmagalhaes@gmail.com>
2020-12-13 19:41:11 -0300

    Update GenericDAO.java

--- src/main/java/br/ufscar/dc/dsw/dao/GenericDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/GenericDAO.java
@@ -9,10 +9,6 @@ abstract public class GenericDAO {
     public GenericDAO() {
         try {
             
-        	/* Setup Banco de dados Derby */
-        	
-        	// Class.forName("org.apache.derby.jdbc.ClientDriver");
-            
         	/* Setup Banco de dados MySQL */
         	
         	Class.forName("com.mysql.cj.jdbc.Driver");
@@ -24,12 +20,8 @@ abstract public class GenericDAO {
 
     protected Connection getConnection() throws SQLException {
     	
-    	/* Conexão banco de dados Derby */
-    	
-    	// return DriverManager.getConnection("jdbc:derby://localhost:1527/Livraria", "root", "root");
-    	
     	/* Conexão banco de dados MySQL */
     	
-    	return DriverManager.getConnection("jdbc:mysql://localhost:3306/Livraria", "root", "alvaro123456");
+    	return DriverManager.getConnection("jdbc:mysql://localhost:3306/ProjetoWeb1", "root", "alvaro123456");
     }
 }
